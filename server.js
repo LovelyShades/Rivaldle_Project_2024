6bbad8dc8186b9301156aab2f30e3ec774bf4714
Alanna Matundan <rosealanna18@gmail.com>
2024-12-30 18:55:58 -0600

    auto open server

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const path = require('path');
+const open = require('open'); // Import the 'open' package
 const app = express();
 const port = 3000;
 
-// Serve the "pages" folder as static
+// Serve the "public" folder as static
 app.use(express.static(path.join(__dirname, 'public')));
+
 // Serve the HTML file
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../pages/home_page/scripts/home.html'));
+  res.sendFile(path.join(__dirname, 'pages/home_page/scripts/home.html'));
 });
 
+// Start the server and open the browser automatically
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
+  open(`http://localhost:${port}`); // Open the URL in the default browser
 });
