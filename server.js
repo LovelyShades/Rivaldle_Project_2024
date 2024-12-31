a3c662ee9c89a853e4ec439b5dffe73420bd7f02
llyons151 <137818217+llyons151@users.noreply.github.com>
2024-12-30 18:56:00 -0600

    open

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Serve the "pages" folder as static
+// Serve the "public" folder as static
 app.use(express.static(path.join(__dirname, 'public')));
-// Serve the HTML file
+
+// Serve the home page HTML file
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../pages/home_page/scripts/home.html'));
+  res.sendFile(path.join(__dirname, 'pages/home_page/home.html'));
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
