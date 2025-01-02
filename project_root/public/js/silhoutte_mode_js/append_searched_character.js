9226f66945196cbbab273130bfea80f8d77af3dc
llyons151 <137818217+llyons151@users.noreply.github.com>
2025-01-02 16:51:39 -0600

    updated appending characters

--- project_root/public/js/silhoutte_mode_js/append_searched_character.js
+++ project_root/public/js/silhoutte_mode_js/append_searched_character.js
@@ -18,8 +18,19 @@ class AppendSearchedCharacter{
         this.searchedCharacterBoxContainer = document.getElementById('searched_characters_container');
         const newBox = document.createElement('div');
         newBox.className = 'searched_character_box';
-        newBox.innerHTML = this.receivedCharacter.name
+
+        const newBoxText = document.createElement('div');
+        newBoxText.className = 'searched_character_text';
+        newBoxText.innerHTML = this.receivedCharacter.name
+
+        const newBoxImg = document.createElement('img');
+        newBoxImg.className = 'searched_character_image';
+        newBoxImg.src = `/_images/character_images/character_icon_images/${this.receivedCharacter.name.replace(/\s+/g, '')}.png`;
+
+
         this.searchedCharacterBoxContainer.prepend(newBox);
+        newBox.append(newBoxImg);
+        newBox.append(newBoxText);
     }
 }
 
