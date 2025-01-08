b7272c8baeacf79883491753cd5a267d6df0ccb9
llyons151 <137818217+llyons151@users.noreply.github.com>
2025-01-08 17:48:56 -0600

    quality chabnges

--- project_root/public/js/ability_mode_js/append_searched_character.js
+++ project_root/public/js/ability_mode_js/append_searched_character.js
@@ -66,7 +66,7 @@ class AppendSearchedCharacter{
         const event = new CustomEvent('correctCharacterGuessed', {
             detail: {
                 character: this.dailyCharacter, // Replace with the actual character
-                mode: 'voice_line' // Replace with the actual mode
+                mode: 'classic' // Replace with the actual mode
             }
         });
         document.dispatchEvent(event); // Dispatch the event with both character and mode
