78b25e82e8cfc0aa85d137e13fb745bae45065c7
llyons151 <137818217+llyons151@users.noreply.github.com>
2025-01-14 11:01:54 -0600

    image cannot be opened in a new tab

--- project_root/public/js/silhoutte_mode_js/append_silhoutte.js
+++ project_root/public/js/silhoutte_mode_js/append_silhoutte.js
@@ -74,6 +74,9 @@ class GetSilhouette {
         if (this.silhouetteImage && character) {
             this.silhouetteImage.style.transform = `scale(${this.imageScale / 10})`;
             this.silhouetteImage.src = `/_images/character_images/hero_profile_images/silhouette_character_image/blackversion/${character}.png`;
+            this.silhouetteImage.addEventListener('contextmenu', (event) => {
+                event.preventDefault();
+            });
         } else {
             console.error('Unable to add silhouette image. Character or image box is missing.');
         }
